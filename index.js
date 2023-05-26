8c1381ca9763975b59b482c80143efcb6faa3211
Liam Conroy Hampton <liam.hampton@hotmail.co.uk>
2023-05-26 11:41:56 +0000

    add handlebars

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+// import express-handlebars
+const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// Configure Handlebars
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+app.set('view engine', 'hbs');
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('main', { message: 'Hello, Handlebars!' });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
